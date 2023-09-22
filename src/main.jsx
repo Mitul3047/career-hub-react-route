a47d5f8c6b6468e82f060d23f96245457f7b4a9b
Shehab Ahamed Chowdhury Mitul <121333557+Mitul3047@users.noreply.github.com>
2023-09-22 11:59:40 +0600

    nav,footer added

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
+import Root from './Components/Root/Root';
+import Home from './Components/Home/Home';
 
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children: [
+      {
+        path : "/",
+        element: <Home></Home>
+      }
+    ]
   },
 ]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
