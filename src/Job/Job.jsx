91d284e88cc83a20e616d7d68586df2c2d93a266
Shehab Ahamed Chowdhury Mitul <121333557+Mitul3047@users.noreply.github.com>
2023-09-22 15:29:00 +0600

    card data loaded and style done

--- src/Job/Job.jsx
+++ src/Job/Job.jsx
@@ -1,11 +1,28 @@
 
 
 const Job = ({ job }) => {
-    const { logo } = job;
+    const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
 
     return (
-        <div>
-<img src={logo} alt="" />
+        <div className="border rounded-lg mt-8">
+            <div className="card card-compact  bg-base-100 shadow-xl">
+                <img src={logo} alt={job_title} className="w-28 ml-4 mt-4" />
+                <div className="card-body">
+                    <h2 className="card-title">{job_title}</h2>
+                    <p>{company_name}</p>
+                    <div className="flex gap-4 ">
+                        <button className="border rounded text-fuchsia-500 border-fuchsia-500 px-5 py-2">{remote_or_onsite}</button>
+                        <button className="border rounded text-fuchsia-500 border-fuchsia-500 px-5 py-2">{job_type}</button>
+                    </div>
+                    <div className="flex gap-6">
+                        <h3>Location: {location}</h3>
+                        <h3>Salary: {salary}</h3>
+                    </div>
+                    <div className="card-actions">
+                        <button className="btn bg-fuchsia-400 text-white">View Details</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
